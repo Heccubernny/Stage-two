8abe44fdf4257362fea3364dc758f7ffcf068a75
Ikubanni Paul <ipom4eva@gmail.com>
2024-07-07 21:25:55 +0100

    deploying

--- app.ts
+++ app.ts
@@ -1,40 +1,3 @@
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import express from 'express';
-// import helmet from 'helmet';
-// import 'reflect-metadata';
-// import { AppDataSource } from './src/data_source';
-// import authRoutes from './src/routes/auth';
-// import organisationRoutes from './src/routes/organisation';
-// import userRoutes from './src/routes/user';
-// dotenv.config();
-
-// const PORT = process.env.PORT || 8000;
-// const app = express();
-// app.use(helmet());
-// app.use(express.json());
-// app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-// app.get('/', async (req, res) => {
-//   return res.status(404).json({ data: 'Home api is working' });
-// });
-
-// AppDataSource.initialize()
-//   .then(() => {
-//     console.log('Connected to the database');
-//     app.use(cors());
-//     app.use('/auth', authRoutes);
-//     app.use('/api/organisations', organisationRoutes);
-//     app.use('/api/users', userRoutes);
-//     app.listen(PORT, () => {
-//       console.log(`Server started on http://localhost:${PORT}`);
-//     });
-//   })
-
-//   .catch((error) => console.log(error));
-
-// export default app;
-
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -52,6 +15,12 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
+app.get('/', (req, res) => {
+  return res.status(404).json({ data: 'Home api is working' });
+});
+app.use('/auth', authRoutes);
+app.use('/api/organisations', organisationRoutes);
+app.use('/api/users', userRoutes);
 
 AppDataSource.initialize()
   .then(() => {
@@ -59,11 +28,4 @@ AppDataSource.initialize()
   })
   .catch((error) => console.log(error));
 
-app.get('/', async (req, res) => {
-  return res.status(404).json({ data: 'Home api is working' });
-});
-
-app.use('/auth', authRoutes);
-app.use('/api/organisations', organisationRoutes);
-app.use('/api/users', userRoutes);
 export default app;
